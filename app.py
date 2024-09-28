a270d7fad3d27bcbda0ac44c22e2df0d6790db4c
root <root@ip-10-0-136-4.ap-northeast-2.compute.internal>
2024-09-28 11:31:16 +0000

    Blue/Green Test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello Cloud! - Continuous_Deployment'
+    return 'Hello Cloud! - Blue_Deployment'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
