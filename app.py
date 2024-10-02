a04e0dc20486c3dafad4674361076221ca15a1ce
root <root@ip-10-0-128-229.ap-northeast-2.compute.internal>
2024-10-02 03:53:36 +0000

    CICD Test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello Cloud! - Version_1'
+    return 'Hello Cloud! - CICD_Pipeline'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
