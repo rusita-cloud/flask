d2271dd32e24efbb8c7a938cd0ab9768e55ba115
root <root@ip-10-0-133-158.ap-northeast-2.compute.internal>
2025-04-24 06:57:50 +0000

    Continuous Deployment Test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello Cloud! - CICD_Pipeline'
+    return 'Hello Cloud! - Continuous_Deployment'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
